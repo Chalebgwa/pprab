786e59d49b07845ccfcdfd3b697c8d139302d750
chalebgwa <blackcrown.bc@gmail.com>
2023-09-04 09:30:00 +0200

    save

---
+++ lib/views/codes_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CodesView extends StatelessWidget {
+  const CodesView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
